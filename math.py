2f33d64e151907b58e92e1ce33b407859a844c9a
gowri <gowri_ananthula@persistent.com>
2022-01-02 22:21:37 +0530

    Added fn headers

--- math.py
+++ math.py
@@ -1,15 +1,19 @@
 #Add Implementation
+#Addition
 def add(x,y):
 	return x+y
 
 #Subtract Implementation
+#Subtraction
 def subtract(x,y):
 	return x-y      #on master branch
 
+#Multiplication
 #Multiply Implementation
 def multiply(x,y):
 	return x*y        #on Bug456 branch
 
+#Division
 #Divide Implementation
 def divide(x,y):
 	if y==0:         #on master branch
